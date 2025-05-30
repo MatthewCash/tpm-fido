cc3e0a5b23f2958aec500e85bc593be31f593167
Matthew_Cash <matthew@matthew-cash.com>
2025-05-30 11:09:27 -0700

    Use buildGoModule instead of gomod2nix

--- flake.nix
+++ flake.nix
@@ -3,26 +3,26 @@
 
     inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     inputs.flake-utils.url = "github:numtide/flake-utils";
-    inputs.gomod2nix.url = "github:nix-community/gomod2nix";
 
-    outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
         let
             pkgs = import nixpkgs {
                 inherit system;
-                overlays = [ gomod2nix.overlays.default ];
             };
-            inherit (pkgs) lib;
         in {
-            packages.default = pkgs.callPackage ./. { };
-            devShells.default = import ./shell.nix { inherit pkgs; };
+            packages.default =pkgs.buildGoModule {
+                pname = "tpm-fido";
+                vendorHash="sha256-qm/iDc9tnphQ4qooufpzzX7s4dbnUbR9J5L770qXw8Y=";
+                version = "0.1";
+                src = ./.;
+            };
         }) // (rec {
         nixosModules.home-manager.tpm-fido = { config, pkgs, lib, system, ... }: {
             options.services."tpm-fido" = {
                 enable = lib.mkEnableOption (lib.mdDoc "Enable tpm-fido service");
                 extraPackages = lib.mkOption {
                     type = with lib.types; listOf package;
-                    default = with pkgs; [ ];
                     description = lib.mdDoc "Extra packages to be used by tpm-fido such as pinentry flavors";
                     example = "with pkgs; [ pinentry-gnome ]";
                 };
